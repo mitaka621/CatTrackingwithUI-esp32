6167d17823e5ace23e0df0dfbe21ef06e869bccc
mitaka621 <d418201@gmail.com>
2023-10-22 16:55:45 +0300

    added new animations, polished design

--- UI/editor.js
+++ UI/editor.js
@@ -26,12 +26,30 @@ function AddEventListeners() {
 }
 
 function EditMap() {
+  const btn = document.querySelector(".button");
+
+  btn.style.maxWidth = "0";
+  document.querySelector(".container").style.height = "0";
+  document.querySelector(".button2.green").style.maxHeight = "0";
+  document.querySelector(".button2.green").style.padding = "0";
+  document.querySelector(".container2").style.height = "50%";
+  document.querySelector(".container3").style.width = "100%";
+  document.querySelector(".container3").style.height = "50%";
+  document.querySelectorAll(".button2")[1].removeEventListener("click", unwrap);
+
+  setTimeout(() => {
+    btn.querySelector("p").innerHTML =
+      '<p><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M177.5 414c-8.8 3.8-19 2-26-4.6l-144-136C2.7 268.9 0 262.6 0 256s2.7-12.9 7.5-17.4l144-136c7-6.6 17.2-8.4 26-4.6s14.5 12.5 14.5 22l0 72 288 0c17.7 0 32 14.3 32 32l0 64c0 17.7-14.3 32-32 32l-288 0 0 72c0 9.6-5.7 18.2-14.5 22z"/></svg>Close Editor<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M177.5 414c-8.8 3.8-19 2-26-4.6l-144-136C2.7 268.9 0 262.6 0 256s2.7-12.9 7.5-17.4l144-136c7-6.6 17.2-8.4 26-4.6s14.5 12.5 14.5 22l0 72 288 0c17.7 0 32 14.3 32 32l0 64c0 17.7-14.3 32-32 32l-288 0 0 72c0 9.6-5.7 18.2-14.5 22z"/></svg></p>';
+
+    btn.style.order = "5";
+    btn.style.maxWidth = "41px";
+  }, 400);
+
   const map = document.querySelector(".map");
   map.setAttribute("class", "map edit");
-  document.querySelector(".sidemenu").innerHTML +=
-    ' <a class="button" onclick="SnapToGrid()" href="#">Snap to Grid</a><a class="button" onclick="AddNewBlock()" href="#">Add New Block</a><a class="button" onclick="Deleteblock()" href="#">Delete Block</a><a class="button" onclick="Save()" href="#">Save</a><a class="button" onclick="Load()" href="#">Load</a>';
+  //document.querySelector(".sidemenu").innerHTML +=
+  //' <a class="button" onclick="SnapToGrid()" href="#">Snap to Grid</a><a class="button" onclick="AddNewBlock()" href="#">Add New Block</a><a class="button" onclick="Deleteblock()" href="#">Delete Block</a><a class="button" onclick="Save()" href="#">Save</a><a class="button" onclick="Load()" href="#">Load</a>';
 
-  const btn = document.querySelector(".button");
   btn.removeEventListener("click", EditMap);
   btn.addEventListener("click", ExitEditor);
 
@@ -44,6 +62,23 @@ function EditMap() {
 }
 
 function ExitEditor() {
+  const btn = document.querySelector(".button");
+  btn.style.maxWidth = "0";
+  document.querySelector(".container").style.height = "100%";
+  document.querySelector(".button2.green").style.maxHeight = "initial";
+  document.querySelector(".button2.green").style.padding = "initial";
+  document.querySelector(".container2").style.height = "0";
+  document.querySelector(".container3").style.width = "0";
+  document.querySelector(".container3").style.height = "0";
+  document.querySelectorAll(".button2")[1].addEventListener("click", unwrap);
+
+  setTimeout(() => {
+    btn.style.order = "0";
+    btn.querySelector("p").innerHTML =
+      '<p> <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" > <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> <style> svg { fill: #ffffff; } </style> <path d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z" /> </svg> Edit Map <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" > <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> <style> svg { fill: #ffffff; } </style> <path d="M334.5 414c8.8 3.8 19 2 26-4.6l144-136c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22l0 72L32 192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32l288 0 0 72c0 9.6 5.7 18.2 14.5 22z" /> </svg> </p>';
+
+    btn.style.maxWidth = "41px";
+  }, 300);
   while (
     Array.from(
       document.querySelector(".sidemenu").querySelectorAll(".sidemenu>.button")
